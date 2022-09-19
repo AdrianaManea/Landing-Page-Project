cff88f93544597655a8306964ffa23a781b6f7a5
Adriana Manea <adrianna.manea@gmail.com>
2022-09-19 15:29:44 +0200

    testimonials section

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Landing page project
+ - The Odin Project Challenge
+
+## Source
+  - [Unsplash Nasa](https://unsplash.com/photos/)
+  - [Kennedy Space Center](https://www.kennedyspacecenter.com/blog/six-astronaut-quotes-from-milestone-nasa-missions)
---
+++ script.js
@@ -0,0 +1,53 @@
+const testimonialContainer = document.querySelector('.testimonial-container');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const username = document.querySelector('.user-name');
+const role = document.querySelector('.role');
+
+const testimonials = [
+  {
+    name: 'Alan Shepard, 1961',
+    position: 'Astronaut',
+    photo: './img/mision-mileston-astronaut-alan-b-shepard.jpeg',
+    text: 'Fix your little problem and light this candle.',
+  },
+  {
+    name: 'Neil Armstrong, 1969',
+    position: 'Astronaut',
+    photo: './img/mission-milestone-astronaut-edwin-aldrin-moon-landing.jpeg',
+    text: 'That\'s one small step for man, one giant leap for mankind.',
+  },
+  {
+    name: 'Sally Ride, 1983',
+    position: 'Astronaut',
+    photo: './img/mission-milestone-sally-ride-sts-7.jpeg',
+    text: 'The starts don\'t look bigger, but they do look brighter.',
+  },
+  {
+    name: 'John Young, 1981',
+    position: 'Astronaut',
+    photo: './img/mission-milestone-sts-1-returns-home.jpeg',
+    text: 'The dream is alive.',
+  },
+]
+
+let idx = 1
+
+function updateTestimonials() {
+  // console.log(testimonials[1]);
+  const { name, position, photo, text } = testimonials[idx]
+
+  testimonial.innerHTML = text
+  userImage.src = photo
+  username.innerHTML = name
+  role.innerHTML = position
+
+  idx++
+
+  // when get to the end, start over
+  if (idx > testimonials.length - 1) {
+    idx = 0
+  }
+}
+
+setInterval(updateTestimonials, 10000)
